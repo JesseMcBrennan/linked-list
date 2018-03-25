85fe8c7478ea499979f9b364388ceddcfa316eed
Chris Boylen <billpolster@gmail.com>
2018-03-25 10:20:27 -0600

    Add var to JS

---
+++ script.js
@@ -0,0 +1,21 @@
+//
+var bookmarkTitle = document.queryselector('#title-input');
+var websiteUrl = document.queryselector('#url-input');
+var submitButton = document.queryselector('#enter');
+
+var outputWebTitle = document.queryselector('.output-title');
+var outputWebUrl = document.queryselector('.output-url');
+var readButton = document.queryselector('.read-button');
+var deleteButton = document.queryselector('.delete-button');
+
+bookmarkTitle.addEventListener('input', function() {
+
+});
+
+websiteUrl.addEventListener('input', function() {
+
+});
+
+submitButton.addEventListener('input', function() {
+
+});
